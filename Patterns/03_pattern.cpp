5804a72ea0462c185b38c9fb9fd9d0cde153de2b
prince-demo <pj2125140@gamil.com>
2026-09-03 10:50:26 +0530

    Added C++ pattern programs

---
+++ Patterns/03_pattern.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printPattern(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of rows: ";
+    cin >> n;
+    printPattern(n);
+    return 0;
+}
+
+
+
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5    
